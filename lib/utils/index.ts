0cf66aacff32b60bc9551a7bccbb21400ad2ba0b
Todd Miller <todd@themillertribe.org>
2026-04-18 14:53:42 -0700

    Add framer-motion and utils index

---
+++ lib/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './computeSimilarity';
+export * from './files';
+export * from './formatHistory';
+export * from './hash';
+export * from './jaccardSim';
+export * from './splitText';
