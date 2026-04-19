78c14276509cc3c91a5c5a204376d01f2c919e26
Todd Miller <todd@themillertribe.org>
2026-04-18 21:18:13 -0700

    Mark native modules as external to avoid build errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['@napi-rs/canvas', 'pdf-parse', 'officeparser', 'pdfjs-dist'],
 };
 
 export default nextConfig;
